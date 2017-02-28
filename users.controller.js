ee54e6f31ff6d6e8df63b0c6d97e78c2cb0d9f52
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-02-28 11:39:20 +0100

    init

--- users.controller.js
+++ users.controller.js
@@ -1,10 +1,4 @@
-angular.module('app').controller('UsersCtrl', function ($scope, Users) {
-
-    $scope.listaUtenti = Users.list();
-
-    $scope.lingua = "Tutte";
-
-    $scope.sesso = "";
+angular.module('app').controller('UsersCtrl', function ($scope) {
 
     $scope.titolo = "lista utenti";
 
